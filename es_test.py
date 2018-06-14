09765f7fc2af85fb1c77b7ed8b32352ac41b609d
amisyy <amisyy@sina.cn>
2018-06-14 09:14:28 +0800

    first commit

---
+++ es_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+# encoding: utf-8
+
+"""
+ 
+@author: yuyingchen
+ 
+@contact: amisyy@sina.cn
+ 
+@software: pycharm
+ 
+@file: es_test.py
+ 
+@time: 2018/5/22 16:54
+"""
+
+from datetime import datetime
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch()
+
